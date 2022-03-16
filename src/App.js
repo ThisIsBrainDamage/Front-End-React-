4eba1ef0085d404215c33ee741061f817c70d2a4
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-03-17 11:36:27 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to <code>reload the local host</code>
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is Dhruv's react project :)
         </a>
       </header>
     </div>
